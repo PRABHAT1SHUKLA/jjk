c07b77604c3f91700af27e96e90819ed2e19fc14
prabhat <prabhatshukla0849@gmail.com>
2024-11-23 18:37:04 +0530

    initialized a basic prismaClient in my project

---
+++ src/index.ts
@@ -0,0 +1,114 @@
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken"
+import z from "zod"
+import bcrypt from 'bcrypt'
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = 3000
+const JWT_SECRET= "my_secret"
+
+interface User {
+  username: string
+  password: string
+}
+
+const users: User[] = []
+
+const signupBody = z.object({
+  username: z.string().min(1, 'Name is required'),
+  password: z.string().min(8, 'Password length should be minimum of 8')
+})
+
+const signinBody = z.object({
+  username: z.string().min(1, 'Name is required'),
+  password: z.string().min(8, 'Password length should be minimum of 8')
+})
+
+app.post('/signup', async (req: Request, res: Response) => {
+
+  try {
+    const body = req.body
+    const result = signupBody.parse(body)
+
+    const { username, password , email} = result
+
+    let foundUser;
+
+    foundUser = users.find((user) => (user.username == username))
+
+    if (foundUser) {
+      res.status(400).send({ message: 'User already exists' })
+    }
+
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    users.push({
+      username: username,
+      password: hashedPassword
+    })
+
+    res.status(200).send({ message: 'User successfully registered' })
+
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      res.status(400).send({ errors: error.errors });
+    } else {
+      res.status(500).send({ message: 'Internal server error' });
+    }
+
+  }
+
+
+
+
+
+})
+
+
+app.post('/signin' , async(req:Request , res:Response)=>{
+  try {
+    const body = req.body 
+    const result = signinBody.parse(body)
+    const { username , password } = result
+
+    const foundUser = users.find((user)=>user.username == username )
+
+    if(!foundUser){
+      res.status(400).send({message: 'Invalid username '})
+    }
+
+    const isPasswordValid =  await bcrypt.compare( password ,foundUser!.password)
+
+    if (!isPasswordValid) {
+      res.status(400).send({ message: 'Invalid  password' });
+      return;
+    }
+
+    const token = jwt.sign({username} , JWT_SECRET , {expiresIn:'1h'})
+
+    res.status(200).send({
+      token ,
+      message: 'User successfully signed in'
+    })
+
+
+  }catch(error){
+    if (error instanceof z.ZodError) {
+      res.status(400).send({ errors: error.errors });
+    } else {
+      res.status(500).send({ message: 'Internal server error' });
+    }
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
